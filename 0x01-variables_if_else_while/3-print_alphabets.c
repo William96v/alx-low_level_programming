008d33ec168328e5be812afeae015a62dff83a1e
William <williamvulandi@icloud.com>
2023-08-11 17:22:58 -0700

    my commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: print alphabets
+ *              
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lowercase = 'a';
+char uppercase = 'A';
+
+while (lowercase <= 'z')
+{
+putchar(lowercase);
+lowercase++;
+}
+
+while (uppercase <= 'Z')
+{
+putchar(uppercase);
+uppercase++;
+}
+putchar('\n');
+
+return (0);
+}
