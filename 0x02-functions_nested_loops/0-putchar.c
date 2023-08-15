f871f588d860273424aeb5cfd5dd44ea2f8c1b0d
William <williamvulandi@icloud.com>
2023-08-15 16:46:05 -0700

    my commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
 #include "main.h"
-/** * main - Prints _putchar followed by a newline * * Return: Always 0. */
+/**
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
 int main(void)
 {
         int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
         int count, sz;
+
         sz = sizeof(str) / sizeof(int);
-        for (count = 0; count < sz; count++){
-                _putchar(str[count]);
-        } _putchar('\n');
+
+        for (count = 0; count < sz; count++)
+        _putchar(str[count]);
+         _putchar('\n');
         return (0);
 }
+
 
