04452d0f7cd726eae89754167d183e63cba21280
William <williamvulandi@icloud.com>
2023-09-07 12:55:45 -0700

    Malloc free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,3 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-
 int _putchar(char c);
 
 void *malloc_checked(unsigned int b);
@@ -12,5 +9,3 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size
-
-#endif
